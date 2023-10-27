e0b2903d9a2dd24fdcb6d96f6659efdc56f49d03
AbdullahAlMomen <momenshovon21@gmail.com>
2023-10-27 19:32:55 +0600

    Add project files.

---
+++ ProblemSolving/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+int m = 1;
+for (int i = 0;i < n; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+    arr[i] = input;
+    if (arr[i] != 0)
+        m = m * arr[i];
+}
+
+for(int i = 0;i < n; i++)
+{
+    if (arr[i] != 0)
+        arr[i] = m / arr[i];
+}
+
+foreach(int i in arr)
+{
+    Console.WriteLine(i);
+}
