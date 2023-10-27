bd32f8243de4752d544252a4075a43b4bc415326
AbdullahAlMomen <momenshovon21@gmail.com>
2023-10-27 20:00:00 +0600

    Tow Sum Problem Add

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -3,24 +3,4 @@
  * Time Complexity:O(n)
  */
 
-int[] TwoSum(int[] nums, int target)
-{
-    
-    Dictionary<int, int> map = new Dictionary<int, int>();
-
-    for (int i = 0; i < nums.Length; i++)
-    {
-        if (map.ContainsKey(nums[i]))
-        {
-            return new int[] { map[nums[i]],i};
-        }
-        else
-        {
-            map[target - nums[i]] = i;
-        }
-    }
-    return new int[] {};
-}
-
-int[] ans = TwoSum(new int[]{ 2, 7, 11, 15 },9);
 
---
+++ TwoSum/TwoSums.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+     class TwoSums
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    return new int[] { map[nums[i]], i };
+                }
+                else
+                {
+                    map[target - nums[i]] = i;
+                }
+            }
+            return new int[] { };
+        }
+        
+        
+
+    }
+
+    public class main
+    {
+        int[] ans = new TwoSums().TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+    }
+}
