fcc0da4472ab8975881e236578496f7a5f08b3ba
Abdullah Al Momen <abdullah.momen@sslwireless.com>
2025-01-13 12:21:41 +0600

    Refactor BasicMath.cs and update methods
    Refactored `BasicMath.cs` by removing unused `using` directives and reformatting method braces. Updated `Practise` method to calculate GCD and LCM of two numbers. Added new methods: `ArmstrongNumber`, `PrintallDivisors`, and `isPrime`. Cleaned up `Program.cs` by removing old test code and adding a call to `BasicMath.isPrime(3)`.

--- BasicMath/BasicMath.cs
+++ BasicMath/BasicMath.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BasicMathProblems
+﻿namespace BasicMathProblems
 {
     public static class BasicMath
     {
@@ -20,40 +14,105 @@ namespace BasicMathProblems
             }
             Console.WriteLine(count);
         }
-        public static int ReverseDigitsofANumber(int n) {
+        public static int ReverseDigitsofANumber(int n)
+        {
 
             int reverseNumber = 0;
 
-            while (n > 0) {
+            while (n > 0)
+            {
                 int z = n % 10;
                 reverseNumber = reverseNumber * 10 + z;
                 n = n / 10;
             }
             Console.WriteLine(reverseNumber);
             return reverseNumber;
-        
+
         }
         public static void CheckPalindrome(int n)
         {
-            int rev=ReverseDigitsofANumber(n);
-            if(n==rev)
+            int rev = ReverseDigitsofANumber(n);
+            if (n == rev)
                 Console.WriteLine("Palindrome Number");
             else Console.WriteLine("Not a Palindrome Number");
         }
 
 
-        public static void Practise(int n)
+        public static void Practise(int n1, int n2)
         {
-            int result = 0;
-            Console.WriteLine(n);
-            while (n > 0)
+            int temp1 = 0; int temp2 = 0;
+            if (n1 > n2)
             {
-                int temp = n % 10;
-                result = result * 10 + temp;
-                n = n / 10;
-                Console.WriteLine(n);
+                temp1 = n1;
+                temp2 = n2;
             }
-            Console.WriteLine(result);
+            else
+            {
+                temp1 = n2;
+                temp2 = n1;
+            }
+            int rem = 0;
+            while (temp2 != 0)
+            {
+                rem = temp1 % temp2;
+                temp1 = temp2;
+                temp2 = rem;
+            }
+            Console.WriteLine(temp1);
+
+            Console.WriteLine((n1 * n2) / temp1);
+
+        }
+
+        public static void ArmstrongNumber(int n)
+        {
+            int count = 0;
+            int temp = n;
+            while (temp != 0)
+            {
+                temp = temp / 10;
+                count++;
+            }
+
+            temp = n;
+            double sum = 0;
+            while (temp != 0)
+            {
+                int rem = temp % 10;
+                sum = sum + Math.Pow(rem, count);
+                temp = temp / 10;
+
+            }
+            if (sum == n)
+                Console.WriteLine(true);
+            Console.WriteLine(false);
+        }
+        public static void PrintallDivisors(int n)
+        {
+            List<int> result = new List<int>();
+            for (int i = 1; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                    result.Add(i);
+            }
+            result.Add(n);
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        public static void isPrime(int n)
+        {
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine("Not a Prime Number");
+                    return;
+                }
+            }
+            Console.WriteLine("is a Prime Number");
         }
     }
 }
